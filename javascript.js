90209217e50a6f8ead6d87bd5ce8dc53cd38df69
Jamie Roth <roth.j@me.com>
2013-02-09 11:22:09 -0500

    Add enter to add new td, add hide/show delete button

--- javascript.js
+++ javascript.js
@@ -1,14 +1,47 @@
-$('document').ready(function(){
-	//Do nothing until page is fully loaded
+$(document).ready(function() {
 
-$('#list td input').onCase('checked'){
-	$('#list td input span').css('text-decoration: line-through');
-}
+// $('#list span').click(function(){
+// 	$(this).dblClick(function(){
 
-$('.arrow-down').click(function(){
-	// Roll over arrow to insert tr
-})
+// 	});
+// });
 
-$('list form td').key
-	// Insert a new tr when editing td and enter keystroke occurs
-});
+// Add class .checked for strikethrough when item is checked
+ $('#list td input').change(function() {
+		if (this.checked) {
+			$(this).parent().addClass('checked');
+		} else {
+			$(this).parent().removeClass('checked');
+		};// internal if
+}); // change listener
+
+
+// $('.arrow-down').click(function(){
+// 	// Roll over arrow to insert tr
+// })
+
+
+// Insert a new tr when editing td and enter keystroke occurs
+// TODO Make this affect new item rows also
+	$('#list td span').keypress(function(event){
+	var keycode = (event.keycode ? event.keycode : event.which);
+	if (keycode =='13'){
+ 		event.preventDefault();
+ 		$('#list').after('<tr><td class="list_item"><input type="checkbox" name="item"  value ="New Item"> <span contenteditable="true">New Item</span><h4>x</h4></td></tr>');
+ 	}; //internal if
+  }); //keypress function
+
+// $('#list td').hover(function() {$('#list h4').show(); $('#list h4').hide();});
+
+
+
+ 
+
+
+// var selectText = function(){
+// 	this.execCommand('selectAll',false, null);
+// };
+
+}); // doc ready
+
+
