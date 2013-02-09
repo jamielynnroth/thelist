de99e84b45c6cb61bdb635c5fbd586aae3e1157c
Jamie Roth <roth.j@me.com>
2013-02-08 20:38:08 -0500

    Add contenteditable

---
+++ javascript.js
@@ -0,0 +1,14 @@
+$('document').ready(function(){
+	//Do nothing until page is fully loaded
+
+$('#list td input').onCase('checked'){
+	$('#list td input span').css('text-decoration: line-through');
+}
+
+$('.arrow-down').click(function(){
+	// Roll over arrow to insert tr
+})
+
+$('list form td').key
+	// Insert a new tr when editing td and enter keystroke occurs
+});
