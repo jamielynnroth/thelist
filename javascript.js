9a51dd9bb35a9cd34e86c2f8f81cc4f9c2459887
Jamie Roth <roth.j@me.com>
2013-02-10 12:48:42 -0500

    Add and remove item features

--- javascript.js
+++ javascript.js
@@ -1,13 +1,7 @@
 $(document).ready(function() {
 
-// $('#list span').click(function(){
-// 	$(this).dblClick(function(){
-
-// 	});
-// });
-
 // Add class .checked for strikethrough when item is checked
- $('#list td input').change(function() {
+ $('#list li input').change(function() {
 		if (this.checked) {
 			$(this).parent().addClass('checked');
 		} else {
@@ -15,15 +9,15 @@ $(document).ready(function() {
 		};// internal if
 }); // change listener
 
-
-// $('.arrow-down').click(function(){
-// 	// Roll over arrow to insert tr
-// })
+// Roll over arrow to insert tr
+$('.arrow-down').click(function(){
+	$('#list').after('<tr><td class="list_item"><input type="checkbox" name="item"  value ="New Item"> <span contenteditable="true">New Item</span><h4>x</h4></td></tr>');
+})
 
 
 // Insert a new tr when editing td and enter keystroke occurs
 // TODO Make this affect new item rows also
-	$('#list td span').keypress(function(event){
+	$(this).find('#list li span').keypress(function(event){
 	var keycode = (event.keycode ? event.keycode : event.which);
 	if (keycode =='13'){
  		event.preventDefault();
@@ -31,17 +25,52 @@ $(document).ready(function() {
  	}; //internal if
   }); //keypress function
 
-// $('#list td').hover(function() {$('#list h4').show(); $('#list h4').hide();});
 
+// Show h4 upon rollover of each li, underline span on rollover
+$('#list li').hover(
+	function() {
+		$(this).find('h4').removeClass('toggle');
+		$(this).find('span').addClass('hover_span')
+}, function () {
+		$(this).find('h4').addClass('toggle');
+		$(this).find('span').removeClass('hover_span')
+}); //hover
+			
 
+//Remove li when h4 is clicked
+$('h4').click(function(event){
+	$(this).parent().addClass('toggle');
+});
 
- 
+// Underline #clear and #remove when you hover over them
+$('#clear').hover(
+	function() {
+		$(this).find('span').addClass('hover_span')
+	}, function() {
+		$(this).find('span').removeClass('hover_span')
+});
 
+$('#remove').hover(
+	function() {
+		$(this).find('span').addClass('hover_span')
+	}, function() {
+		$(this).find('span').removeClass('hover_span')
+});
 
-// var selectText = function(){
-// 	this.execCommand('selectAll',false, null);
-// };
+// Remove all li when "Clear list" is clicked
+$('#clear').click(function(){
+	var response= confirm("Are you sure you want to reset the list?");
+	if (response==true) {
+		$('#list li').remove();
+	} else {};	
+})
+
+// Remove all checked li when Remove Checked is clicked
+$('#remove').click(function(){
+	$('#list li input:checked').parent().remove();
+});
 
 }); // doc ready
+
 
 
